fe6aebc98f099aa84bcb04c8aabb9d3606d0352f
Alan Gutierrez <alan@blogometer.com>
2009-03-13 20:09:14 -0500

    - Using Ilk.Pair and not using UncheckedCast.

--- src/main/java/com/goodworkalan/stash/Stash.java
+++ src/main/java/com/goodworkalan/stash/Stash.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.goodworkalan.ilk.Ilk;
-import com.goodworkalan.ilk.UncheckedCast;
 
 /**
  * A type-safe heterogeneous collection. 
@@ -18,31 +17,7 @@ public class Stash implements Serializable
     private static final long serialVersionUID = 1L;
 
     /** Map of keys to type mapped objects. */
-    private final Map<Key, Map<Ilk.Key, Object>> map = new HashMap<Key, Map<Ilk.Key, Object>>();
-
-    /**
-     * Map the given value of the type given by the <code>Ilk.Key</code> to the
-     * given key.
-     * 
-     * @param <T>
-     *            The type of the value.
-     * @param key
-     *            The key.
-     * @param ilkKey
-     *            The type token.
-     * @param value
-     *            The value.
-     */
-    private <T> void put(Key key, Ilk.Key ilkKey, T value)
-    {
-        Map<Ilk.Key, Object> favorites = map.get(key);
-        if (favorites == null)
-        {
-            favorites = new HashMap<Ilk.Key, Object>();
-            map.put(key, favorites);
-        }
-        favorites.put(ilkKey, value);
-    }
+    private final Map<Key, Ilk.Pair> map = new HashMap<Key, Ilk.Pair>();
 
     /**
      * Map the given value of the type given by the <code>Ilk</code> super type
@@ -59,7 +34,7 @@ public class Stash implements Serializable
      */
     public <T> void put(Key key, Ilk<T> ilk, T value)
     {
-        put(key, ilk.key, value);
+        map.put(key, ilk.pair(value));
     }
     
     /**
@@ -69,34 +44,14 @@ public class Stash implements Serializable
      *            The type of the value.
      * @param key
      *            The key.
-     * @param typeClass
+     * @param type
      *            The class of the value.
      * @param value
      *            The value.
      */
-    public <T> void put(Key key, Class<T> typeClass, T value)
+    public <T> void put(Key key, Class<T> type, T value)
     {
-        put(key, new Ilk.Key(typeClass), value);
-    }
-
-    /**
-     * Get the value of the given key and the type given by the
-     * <code>Ilk.Key</code>.
-     * 
-     * @param key
-     *            The key.
-     * @param typeKey
-     *            The type token of the value.
-     * @return The value or null if none exists in the container.
-     */
-    private Object get(Key key, Ilk.Key typeKey)
-    {
-        Map<Ilk.Key, Object> favorites = map.get(key);
-        if (favorites == null)
-        {
-            return null;
-        }
-        return favorites.get(typeKey);
+        put(key, new Ilk<T>(type), value);
     }
 
     /**
@@ -111,7 +66,12 @@ public class Stash implements Serializable
      */
     public <T> T get(Key key, Ilk<T> ilk)
     {
-        return new UncheckedCast<T>().cast(get(key, ilk.key)); 
+        Ilk.Pair pair = map.get(ilk.key);
+        if (pair == null)
+        {
+            return null;
+        }
+        return pair.cast(ilk);
     }
 
     /**
@@ -119,13 +79,13 @@ public class Stash implements Serializable
      * 
      * @param key
      *            The key.
-     * @param typeClass
+     * @param type
      *            The class of the type.
      * @return The value or null if none exists in the container.
      */
-    public <T> T get(Key key, Class<T> typeClass)
+    public <T> T get(Key key, Class<T> type)
     {
-        return typeClass.cast(get(key, new Ilk.Key(typeClass)));
+        return get(key, new Ilk<T>(type));
     }
 
     /**
