a8365f28dab75569c01d054329afa53e7be561ec
Alan Gutierrez <alan@blogometer.com>
2010-04-18 12:38:35 -0500

    Test brace tidy.

--- src/test/java/com/goodworkalan/stash/StashTest.java
+++ src/test/java/com/goodworkalan/stash/StashTest.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.stash;
 
-
 import static org.testng.Assert.assertEquals;
 
 import java.util.Arrays;
@@ -10,19 +9,19 @@ import org.testng.annotations.Test;
 
 import com.goodworkalan.ilk.Ilk;
 
-public class StashTest
-{
+public class StashTest {
     @Test
-    public void stash()
-    {
+    public void stash() {
         Stash.Key key = new Stash.Key();
         Stash stash = new Stash();
         stash.put(key, String.class, "Hello, World!");
         assertEquals(stash.get(key, String.class), "Hello, World!");
-        
+
         List<String> strings = Arrays.asList("a", "b", "c");
-        stash.put(key, new Ilk<List<String>>() {}, strings);
-        strings = stash.get(key, new Ilk<List<String>>() { });
+        stash.put(key, new Ilk<List<String>>() {
+        }, strings);
+        strings = stash.get(key, new Ilk<List<String>>() {
+        });
         assertEquals(strings.get(0), "a");
     }
 }
