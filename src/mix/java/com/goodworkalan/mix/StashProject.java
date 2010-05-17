57b21a75b0fd90e931ed87380f36545855e8bd50
Alan Gutierrez <alan@blogometer.com>
2010-05-16 19:14:30 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/mix/StashProject.java
+++ src/mix/java/com/goodworkalan/mix/StashProject.java
@@ -4,21 +4,25 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class StashProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Stash.
+ *
+ * @author Alan Gutierrez
+ */
+public class StashProject implements ProjectModule {
+    /**
+     * Build the project definition for Stash.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.stash/stash/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.ilk/ilk/0.+1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.ilk/ilk/0.+1")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
